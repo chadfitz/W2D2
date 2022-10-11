0f15e222ed5c2ad6ed4b35bba6239714c0ff13bd
chad fitzgerald <fitzchad@gmail.com>
2022-10-11 12:11:59 -0700

    hangman.rb finished

--- hangman_project 2/lib/hangman.rb
+++ hangman_project 2/lib/hangman.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
   # @@random_word = DICTIONARY.sample
@@ -44,4 +46,53 @@ class Hangman
   def fill_indices(char, indices_arr)
     indices_arr.each { |index| @guess_word[index] = char }
   end
+
+  def try_guess(char)
+    if already_attempted?(char)
+      p 'that has already been attempted'
+      return false
+    else
+      @attempted_chars << char
+           
+      if get_matching_indices(char).length >= 1
+        fill_indices(char, get_matching_indices(char))
+      else
+        @remaining_incorrect_guesses -= 1
+      end
+      true
+    end
+  end
+
+  def ask_user_for_guess
+    p 'Enter a char:'
+    input = gets.chomp
+    try_guess(input)
+  end
+
+  def win?
+    if @guess_word.join("") == @secret_word
+      p 'WIN'
+      return true
+    else 
+      false
+    end
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      p 'LOSE'
+      return true
+    else false
+    end
+  end
+
+  def game_over?
+    if win? || lose?
+      p @secret_word
+      return true
+    else
+      false
+    end
+  end
+
 end
